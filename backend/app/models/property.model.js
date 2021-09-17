b895cd9f646ba9b0274016918fa5053ff3cc7eaa
Maahee <58122827+msabur@users.noreply.github.com>
2021-09-17 17:13:33 -0400

    add Property model for database

---
+++ backend/app/models/property.model.js
@@ -0,0 +1,12 @@
+var mongoose = require("mongoose")
+
+const schema = mongoose.Schema({
+	name: String,
+	visibility: {
+        type: String,
+        enum: ['public', 'private'],
+        default: 'private'
+    }
+});
+
+module.exports = mongoose.model("Property", schema);
