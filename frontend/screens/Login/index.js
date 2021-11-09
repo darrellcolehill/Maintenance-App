445523c5e57b6504b1828258720795b7ba2db2dc
Maahee <58122827+msabur@users.noreply.github.com>
2021-11-08 19:04:38 -0500

    Use Grant's login screen

--- frontend/screens/Login/index.js
+++ frontend/screens/Login/index.js
@@ -1,13 +1,18 @@
-import React from "react";
-import { View, StyleSheet } from "react-native";
-import { Button, Text, Title } from "react-native-paper";
-import LoginForm from "./LoginForm";
-import { observer } from "mobx-react";
+import React, { useState } from "react";
+import {
+  ImageBackground,
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+  Text,
+  TextInput,
+  KeyboardAvoidingView,
+} from "react-native";
 import { AuthStore } from "../../stores/auth";
 import * as Api from "../../api";
 
-const Login = observer(function ({ navigation }) {
-  async function handleLogin(username, password) {
+function Login({ navigation }) {
+  async function submitLogin(username, password) {
     AuthStore.startLoading();
     let response = await Api.login(username, password);
     let token = response.token;
@@ -16,23 +21,104 @@ const Login = observer(function ({ navigation }) {
     AuthStore.login(username, token);
   }
 
+  let [username, setUsername] = useState("");
+  let [password, setPassword] = useState("");
+
   return (
-    <View style={styles.container}>
-      <Title>Login</Title>
-      {AuthStore.loading && <Text>LOADING...</Text>}
-      <LoginForm handler={handleLogin} />
-      <Button onPress={() => navigation.navigate("Signup")} mode="contained">
-        Need an account?
-      </Button>
-    </View>
+    <KeyboardAvoidingView style={styles.background} behavior="height">
+      <ImageBackground // the background image
+        style={styles.background}
+        source={require("../../assets/background.jpg")}
+      >
+        <TouchableOpacity // login element
+          style={styles.loginButton}
+          onPress={() => submitLogin(username, password)}
+          activeOpacity={0.8}
+        >
+          <Text style={styles.textFonts}>Login</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity // register account element
+          style={styles.registerButton}
+          onPress={() => navigation.navigate("Signup")}
+          activeOpacity={0.8}
+        >
+          <Text style={styles.textFonts}>Register for a new account!</Text>
+        </TouchableOpacity>
+
+        <TextInput // username box element
+          style={styles.input}
+          onChangeText={text => setUsername(text)}
+          placeholder="Username"
+        ></TextInput>
+
+        <TextInput // password box element
+          style={styles.input}
+          onChangeText={text => setPassword(text)}
+          placeholder="Password"
+          secureTextEntry={true}
+        ></TextInput>
+
+        <Image // logo image
+          style={styles.logo}
+          source={require("../../assets/logo.png")}
+        ></Image>
+      </ImageBackground>
+    </KeyboardAvoidingView>
   );
-});
+}
 
 const styles = StyleSheet.create({
-  container: {
+  background: {
     flex: 1,
+  },
+  input: {
+    position: "relative",
+    top: "40%",
+    alignSelf: "center",
+    borderWidth: 1,
+    width: "70%",
+    height: "5%",
+    backgroundColor: "white",
     padding: 10,
+    margin: 12,
+    flex: 0.0275,
+  },
+
+  loginButton: {
+    width: "80%",
+    height: "7%",
+    alignSelf: "center",
+    backgroundColor: "mediumseagreen",
+    position: "absolute",
+    bottom: "15%",
+    borderRadius: 5,
+    alignItems: "center",
     justifyContent: "center",
+    flex: 1,
+  },
+  logo: {
+    position: "absolute",
+    alignSelf: "center",
+    justifyContent: "center",
+    height: "40%",
+    width: "40%",
+    top: "5%",
+  },
+  registerButton: {
+    width: "80%",
+    height: "7%",
+    alignSelf: "center",
+    backgroundColor: "mediumseagreen",
+    position: "absolute",
+    bottom: "5%",
+    borderRadius: 5,
+    alignItems: "center",
+    justifyContent: "center",
+    flex: 1,
+  },
+  textFonts: {
+    fontSize: 25,
   },
 });
 
