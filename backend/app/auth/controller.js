e8f8ef6db00aca9239b77d7a3fdf5311078f9490
Darrell Cole Hill <darrellcolehill@gmail.com>
2021-11-21 11:19:27 -0500

    updated backend
    added support to add roles to roles table on signup
    
    added support to send username and role data in token payload

--- backend/app/auth/controller.js
+++ backend/app/auth/controller.js
@@ -20,15 +20,28 @@ exports.login = (req, res, next) =>
 		{
 			if (user && password === user.password) 
 			{
-				const payload = {
-					id: user.rowid
-				};
-				const options = {
-					expiresIn: "24 hours"
-				};
-				return res.status(200).json({
-					token: jwt.sign(payload, jwtSecret, options)
+
+				// get role data for specified user
+				db.all("SELECT role, rating FROM roles WHERE username = ?", username)
+				.then(userRoles => 
+				{
+					console.log(userRoles); // TODO: delete after testing
+					
+					const tokenPayload = {
+						id: user.username,
+						roles: userRoles
+					};
+
+					const options = {
+						expiresIn: "24 hours"
+					};
+					return res.status(200).json({
+						token: jwt.sign(tokenPayload, jwtSecret, options),
+					});
+
 				});
+
+				
 			}
 			else 
 			{
@@ -47,8 +60,13 @@ exports.signup = (req, res, next) =>
 {
 	const {username, password, email, isTenant, isLandlord, isHandyman, isHomeowner} = req.body;
 
+	// Array that will be used to help with inserting the user choosen roles
+	var roles = [{setRole: isTenant, value: "TENANT"}, {setRole: isLandlord, value: "LANDLORD"}, 
+				{setRole: isHandyman, value: "HANDYMAN"}, {setRole: isHomeowner, value: "HOMEOWNER"}];
 
-	if (!username || !password || !email) 
+	console.log(roles);
+
+	if (!username || !password || !email) // Ensures that required fields exist
 	{
 		return res.status(400).json({
 			message: "Missing username or password"
@@ -59,7 +77,7 @@ exports.signup = (req, res, next) =>
 	db.get("SELECT (rowid) FROM users WHERE username = ?", username)
 		.then(user => 
 		{
-			if (!user) 
+			if (!user) // Make sure username not taken
 			{
 				db.run(
 					"INSERT INTO users (username, password, email) VALUES (?, ?, ?)",
@@ -67,7 +85,22 @@ exports.signup = (req, res, next) =>
 				)
 					.then(() => 
 					{
-						// TODO: INSERT INTO ROLES TABLE HERE
+						// inserts user choosen roles into roles database
+						for(var i = 0; i < roles.length; i++)
+						{
+							if(roles[i].setRole == true)
+							{
+								db.run(
+									"INSERT INTO roles (username, role, rating) VALUES (?, ?, ?)",
+									[username, roles[i].value, 0.0]
+								)
+								.catch(error => 
+								{
+									return next(error);
+								})
+							}
+								
+						}
 
 						return res.status(200).json({
 							message: "Successfully signed up!"
--- backend/app/database/setupCommands.js
+++ backend/app/database/setupCommands.js
@@ -4,4 +4,21 @@ module.exports = [
         password STRING,
         email STRING
     )`
+
 ];
+
+
+// List of queries used to create tables
+/*
+`CREATE TABLE IF NOT EXISTS users (
+        username STRING,
+        password STRING,
+        email STRING
+    )`
+
+    `CREATE TABLE IF NOT EXISTS roles (
+        username STRING,
+        role STRING,
+        rating FLOAT(5,2)
+    )`
+*/
