b09337c48131ffe9ac65530e28a1abaa6e460ee5
Maahee <58122827+msabur@users.noreply.github.com>
2021-11-22 13:18:19 -0500

    add documentation comment in search route

--- backend/app/posts/routes.js
+++ backend/app/posts/routes.js
@@ -15,6 +15,12 @@ router.post("/makePost",
 	posts.makePost
 );
 
+/**
+ * params: location
+ * This route requires a valid token in the header.
+ * If no valid token given, responds with status 401
+ * on success, responds with status 200
+ */
 router.get("/search",
 	jwt({ secret: config.jwtSecret, algorithms: ["HS256"] }),
 	posts.search
