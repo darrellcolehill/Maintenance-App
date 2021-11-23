e3350f23ff62805a4d174858e1b90b4121c9b422
Grant Linke <GLinke123@gmail.com>
2021-11-22 19:07:11 -0500

    Creates a screen for landlord feed

---
+++ frontend/screens/LandlordFeed/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+import { Text, Button } from "react-native-paper";
+import { AuthStore } from "../../stores/auth";
+
+export function LandlordFeed({ navigation }) {
+  let username = AuthStore.username;
+  return (
+    <View style={styles.container}>
+      <Text>Welcome, {username}!</Text>
+      <Button onPress={() => AuthStore.logout()} mode="contained">
+        Sign out
+      </Button>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
