812108f2d0bf4ad955d3862634f777280bd5aeff
Grant Linke <GLinke123@gmail.com>
2021-11-22 19:10:57 -0500

    Adds searchbar to homepage

--- frontend/screens/Home/index.js
+++ frontend/screens/Home/index.js
@@ -1,12 +1,22 @@
 import React from "react";
+import { SearchBar } from "react-native-elements";
 import { View, StyleSheet } from "react-native";
 import { Text, Button } from "react-native-paper";
 import { AuthStore } from "../../stores/auth";
 
 export function Home({ navigation }) {
   let username = AuthStore.username;
+  let [searchBar, setSearchBar] = useState("");
+  
   return (
     <View style={styles.container}>
+      <SearchBar
+        lightTheme="True"
+        placeholder="Search..."
+        onChangeText={text => setSearchBar(text)}
+        value={searchBar}
+      />
+
       <Text>Welcome, {username}!</Text>
       <Button onPress={() => AuthStore.logout()} mode="contained">
         Sign out
