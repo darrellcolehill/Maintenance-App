295f46b0baef8e1959fbd59a1cb30174aa8a95a4
Darrell Cole Hill <darrellcolehill@gmail.com>
2021-11-23 18:26:02 -0500

    added new ngrok address

--- frontend/api/index.js
+++ frontend/api/index.js
@@ -10,7 +10,7 @@ function delay(ms) {
 
 
 
-const URL = 'http://f686-76-78-236-214.ngrok.io'; // NOTE: for some reason, fetch will not let you use local host
+const URL = 'http://d433-172-58-171-3.ngrok.io'; // NOTE: for some reason, fetch will not let you use local host
 
 
 
