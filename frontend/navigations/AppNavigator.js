9a8599a79ea38c5b21b71b0d61e4fbd0c1b06e98
Grant Linke <GLinke123@gmail.com>
2021-11-22 19:05:08 -0500

    Added navbar icons and search bar

--- frontend/navigations/AppNavigator.js
+++ frontend/navigations/AppNavigator.js
@@ -13,9 +13,11 @@ import { Messages } from "../screens/Messages";
 import { MessageContent } from "../screens/Messages/MessageContent";
 import { NewMessage } from "../screens/Messages/NewMessage"
 import { Profile } from "../screens/Profile";
+import { LandlordFeed } from "../screens/LandlordFeed";
 
 const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
+const isLandlord = true;  // WILL NEED TO IMPLEMENT SERVER-SIDE CODE ONCE COMPLETE HERE!!!
 
 function HomeNav() {
   return (
@@ -43,7 +45,62 @@ function ProfileNav() {
   );
 }
 
+function LandlordFeedNav() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="LandlordFeed" component={LandlordFeed} />
+    </Stack.Navigator>
+  );
+}
+
 function AppNavigator() {
+  if(isLandlord)
+  {
+    return (
+      <Tab.Navigator screenOptions={{ headerShown: false }}>
+        <Tab.Screen
+          name="Home"
+          component={HomeNav}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons name="home" color={color} size={size} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Messages"
+          component={MessagesNav}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons name="message" color={color} size={size} />
+            ),
+          }}
+        />
+        <Tab.Screen
+            name="Feed"
+            component={LandlordFeedNav}
+            options={{
+              tabBarIcon: ({color, size}) => (
+                <MaterialCommunityIcons name="card-text" color={color} size={size} />
+              ),
+            }}
+          />
+        <Tab.Screen
+          name="Profile"
+          component={ProfileNav}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons name="account" color={color} size={size} />
+            ),
+          }}
+        />
+          
+      </Tab.Navigator>
+
+    );
+  }
+
+  else{
   return (
     <Tab.Navigator screenOptions={{ headerShown: false }}>
       <Tab.Screen
@@ -69,12 +126,13 @@ function AppNavigator() {
         component={ProfileNav}
         options={{
           tabBarIcon: ({ color, size }) => (
-            <MaterialCommunityIcons name="message" color={color} size={size} />
+            <MaterialCommunityIcons name="account" color={color} size={size} />
           ),
         }}
       />
     </Tab.Navigator>
   );
+  }
 }
 
 export default AppNavigator;
