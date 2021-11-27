e93090cd333b3827a2f855e349df85b04fd44397
Maahee <58122827+msabur@users.noreply.github.com>
2021-11-27 18:37:08 -0500

    Add landlord personalized feed

--- frontend/screens/LandlordFeed/index.js
+++ frontend/screens/LandlordFeed/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import { View, StyleSheet, FlatList } from "react-native";
-import { Text, Button } from "react-native-paper";
+import { Text, Button, TextInput, Title } from "react-native-paper";
 import { AuthStore } from "../../stores/auth";
 import PostItem from "../Home/PostItem";
 import * as Api from "../../api"
@@ -10,6 +10,22 @@ export function LandlordFeed({ navigation }) {
   let username = AuthStore.username;
   let [posts, setPosts] = useState([])
   let [isRefreshing, setIsRefreshing] = useState(false)
+  let [location, setLocation] = useState("");
+
+  const getPosts = async () => {
+    let data = await Api.getLFeed();
+    if (data.message === "NEED_LOCATION") {
+      setPosts(data.message);
+    } else {
+      setPosts(data.result)
+    }
+    setIsRefreshing(false)
+  }
+
+  const submitLocation = async () => {
+    await Api.setOwnLocation({ location: location });
+    await getPosts();
+  }
 
   const renderItem = ({ item }) => {
     return (
@@ -20,26 +36,30 @@ export function LandlordFeed({ navigation }) {
     );
   };
 
-  const getPosts = async () => {
-    let data = await Api.getLFeed();
-    setPosts(data.result)
-    setIsRefreshing(false)
-  }
-
   useEffect(() => {
     getPosts();
   }, []);
 
   return (
     <View style={styles.container}>
-      <Text>Posts from users in your building:</Text>
-      <FlatList
+      <Title>Posts from users in your building:</Title>
+      {posts === "NEED_LOCATION" ? (
+        <>
+          <Text>Please enter your location.</Text>
+          <TextInput placeholder="Location" onChangeText={text => setLocation(text)} />
+          <Button mode="contained" icon="floppy" onPress={() => submitLocation()}>
+            Save
+          </Button>
+        </>
+      ) : (
+        <FlatList
           data={posts}
           renderItem={renderItem}
           keyExtractor={(item) => item.id.toString()}
           onRefresh={() => { setIsRefreshing(true); getPosts(); }}
           refreshing={isRefreshing}
         />
+      )}
     </View>
   );
 }
