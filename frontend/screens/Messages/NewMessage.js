7e048ab7c2097b183e7734da3f8e7e69ce80a07f
Grant Linke <GLinke123@gmail.com>
2021-11-28 10:45:26 -0500

    Adds popup notifs for success and invalid message sent

--- frontend/screens/Messages/NewMessage.js
+++ frontend/screens/Messages/NewMessage.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { View, StyleSheet, Text } from "react-native";
+import { View, StyleSheet, Text, Alert } from "react-native";
 import { TextInput, Button } from "react-native-paper";
 import * as Api from "../../api";
 import { AuthStore } from "../../stores/auth";
@@ -12,6 +12,23 @@ export function NewMessage({ navigation }) {
   // For test case ID 26 from Test Plan in Deliverable 2
   const charLimit = 200
 
+  const createOneButtonAlertSuccess = () =>
+  Alert.alert(
+    "Message successfully sent!",
+    "We will now redirect you to your inbox.",
+    [
+      { text: "OK", onPress: () => navigation.navigate("Message Inbox") }
+    ]
+  );
+
+  const createOneButtonAlertInvalid = () =>
+  Alert.alert(
+    "User does not exist",
+    "Please check username entered.",
+    [
+      { text: "OK", onPress: () => console.log("Invalid username.") }
+    ]
+  );
 
   async function sendMessage(recipient, content) {
 
@@ -20,12 +37,11 @@ export function NewMessage({ navigation }) {
       let response = await Api.sendMessage(recipient, content);
 
       //TODO: ADD NAVIGATION OR ALERT THAT MESSAGE IS SEND
-        //if(response.message == "Successful") navigation.navigate("Messages");
-        //else navigation.navigate("Signup");
+        if(response.message == "Successful"){ createOneButtonAlertSuccess(); }
+        else createOneButtonAlertInvalid();
 
       AuthStore.stopLoading();
 
-	  navigation.goBack();
   }
 
 
