30a927f8fffbf801a36b90af052c490b519dd820
Smaschin <49967736+smaschin@users.noreply.github.com>
2021-11-28 14:23:39 -0500

    added star library and finished profile layout

--- frontend/screens/Profile/index.js
+++ frontend/screens/Profile/index.js
@@ -1,26 +1,51 @@
-import React from "react";
-import { View, StyleSheet, Image } from "react-native";
-import { Button, Text, Title } from "react-native-paper";
+import React, { useState, useEffect } from "react";
+import { View, StyleSheet, Image,} from "react-native";
+import { Button, Text,} from "react-native-paper";
+import StarRating from 'react-native-star-rating';
 import { AuthStore } from "../../stores/auth";
+import * as Api from "../../api";
 
-// renders each item in the list of messages
-function RatingItem({ item}) {
+//replace with data from database
+const ROLES = ['Tenant', 'Landlord', 'Handyman', 'Homeowner'];
+const SCORES = [2.5, 4.8, 3.2, 1.6];
+const IS_TENANT = true, IS_LANDLORD = true, IS_HANDYMAN = true, IS_HOMEOWNER = true;
+
+var roleCount;
+
+// Renders a rating component
+const Rating = () => {
+  roleCount++;
+  var roleTitle = ROLES[roleCount];
 
   return (
-    <TouchableOpacity>
-
-     <List.Item
-         title={`${item.role}`}
-         description= {`${item.rating}`}
-       />
-
-    </TouchableOpacity>
+      <View style={styles.ratingInfo}>
+        <View style={styles.ratingTitleContainer}>
+          <Text style={styles.ratingTitle}>{roleTitle}</Text>
+        </View>
+          <View style={styles.starContainer}>
+            <StarRating style={styles.stars}
+              maxStars={5}
+              rating={SCORES[roleCount]}
+              starSize={25}
+              emptyStarColor={'slategray'}
+              halfStarColor={'slategray'}
+              fullStarColor={'slategray'}
+            ></StarRating>
+          </View>
+      </View>
   );
 }
 
+// Component for not rendering rating but keeping count
+const MissingRating = () => {
+  roleCount++;
 
-export function Profile() {
+  return null;
+}
+
+export function Profile({ navigation }) {
   let username = AuthStore.username;
+  roleCount = -1;
 
   return (
     <View style={styles.container}>
@@ -29,15 +54,34 @@ export function Profile() {
           <View style={styles.usernameContainer}>
             <Text style={styles.username}>{username}</Text>
           </View>
-          <View style={styles.imageContainer}>
-            <Image style={styles.image} source={{uri: 'https://i.imgur.com/lHeVHrG.png'}}></Image>
+          <View style={styles.profilePicContainer}>
+            <Image style={styles.profilePic} source={{uri: 'https://i.imgur.com/lHeVHrG.png'}}></Image>
           </View>
         </View>
       </View>
+      <View style={styles.ratingContainer}>
+        {IS_TENANT ? <Rating></Rating> : <MissingRating></MissingRating>}
+        {IS_LANDLORD ? <Rating></Rating> : <MissingRating></MissingRating>}
+        {IS_HANDYMAN ? <Rating></Rating> : <MissingRating></MissingRating>}
+        {IS_HOMEOWNER ? <Rating></Rating> : <MissingRating></MissingRating>}
+      </View>
+      {IS_LANDLORD ?
+      <View style={styles.addressButtonContainer}>
+        <Button style={styles.addressButton}>
+          <Text style={styles.addressButtonText}>View Addresses</Text>
+        </Button>
+      </View>
+      : null}
       <View style={styles.logoutContainer}>
+<<<<<<< Updated upstream
       <Button style={styles.logoutButton} onPress={() => AuthStore.logout()} mode="contained">
         <Text style={styles.logoutText}>LOGOUT</Text>
       </Button>
+=======
+        <Button style={styles.logoutButton} onPress={() => AuthStore.logout()} mode="contained">
+          <Text style={styles.logoutText}>LOGOUT</Text>
+        </Button>
+>>>>>>> Stashed changes
       </View>
     </View>
   );
@@ -50,22 +94,23 @@ const styles = StyleSheet.create({
   },
 
   header: {
-    height: "40%",
+    height: '30%',
+    width: '100%',
+    alignItems: "center",
+    justifyContent: "center",
+    paddingBottom: 5,
+  },
+
+  headerBlock: {
+    flex: 1,
     width: "100%",
+    backgroundColor: 'slategray',
     alignItems: "center",
     justifyContent: "center",
   },
 
-  headerBlock: {
-    height: "75%",
-    width: "100%",
-    backgroundColor: 'slategray',
-    alignItems: "center",
-    justifyContent: "flex-start",
-  },
-
   usernameContainer: {
-    height: "25%",
+    flex: .25,
     width: "100%",
     alignItems: "center",
     justifyContent: "center",
@@ -77,26 +122,83 @@ const styles = StyleSheet.create({
     color: "white",
   },
 
-  imageContainer: {
-    height: "75%",
+  profilePicContainer: {
+    flex: .75,
     width: "100%",
     alignItems: "center",
     justifyContent: "center",
   },
 
-  image: {
-    width: 150 ,
-    height: 150,
+  profilePic: {
+    width: 120,
+    height: 120,
+  },
+
+  ratingContainer: {
+    height: '40%',
+    width: '95%',
+    alignSelf: 'center',
+    paddingTop: 5,
+    paddingBottom: 5,
+  },
+
+  ratingInfo: {
+    width: 150,
+    height: 50,
+    alignSelf: 'center',
+  },
+
+  ratingTitleContainer: {
+    height: 20,
+    justifyContent: 'center',
+  },
+
+  ratingTitle: {
+    fontSize: 18,
+    fontWeight: "bold",
+    alignSelf: 'center',
+  },
+
+  starContainer: {
+    alignContent: 'flex-start',
+    alignSelf: 'center',
+    justifyContent: 'center',
+    width: 150,
+    height: 30,
+  },
+
+  stars: {
+    alignSelf: 'center',
+  },
+
+  addressButtonContainer: {
+    height: '15%',
+    justifyContent: 'center',
+    paddingTop: 5,
+    paddingBottom: 5,
+  },
+
+  addressButton: {
+    justifyContent: "center",
+    alignSelf: 'center',
+    backgroundColor: 'slategray',
+  },
+
+  addressButtonText: {
+    fontWeight: 'bold',
+    color: 'white',
   },
 
   logoutContainer: {
-    justifyContent: "flex-end",
+    height: '15%',
+    justifyContent: 'flex-end',
     paddingTop: 5,
     paddingBottom: 5,
   },
 
   logoutButton: {
     backgroundColor: 'slategray',
+    alignSelf: 'center',
   },
 
   logoutText
