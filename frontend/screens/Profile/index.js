9d732de6c518c9fcc7f1d8250d9810fa104a115f
Smaschin <49967736+smaschin@users.noreply.github.com>
2021-11-29 00:27:07 -0500

    Removed old address button

--- frontend/screens/Profile/index.js
+++ frontend/screens/Profile/index.js
@@ -76,13 +76,6 @@ const renderItem = ({ item }) => {
           keyExtractor={(item) => item.id.toString()}
         />
       </View>
-      <View style={styles.addressButtonContainer}>
-      {AuthStore.isLandlord ?
-        <Button style={styles.addressButton}>
-          <Text style={styles.addressButtonText}>View Addresses</Text>
-        </Button>
-      : null}
-      </View>
       <View style={styles.logoutContainer}>
         <Button style={styles.logoutButton} onPress={() => AuthStore.logout()} mode="contained">
           <Text style={styles.logoutText}>LOGOUT</Text>
@@ -174,24 +167,6 @@ const styles = StyleSheet.create({
 
   stars: {
     alignSelf: 'center',
-  },
-
-  addressButtonContainer: {
-    height: '15%',
-    justifyContent: 'center',
-    paddingTop: 5,
-    paddingBottom: 5,
-  },
-
-  addressButton: {
-    justifyContent: "center",
-    alignSelf: 'center',
-    backgroundColor: 'slategray',
-  },
-
-  addressButtonText: {
-    fontWeight: 'bold',
-    color: 'white',
   },
 
   logoutContainer: {
